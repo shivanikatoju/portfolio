7ab1bf0c1c5d00c16ba4922c2acf764a8a01de71
= <=>
2024-03-15 10:48:28 +1100

    hero changed

---
+++ Script.js
@@ -0,0 +1,20 @@
+// function pick() {
+//   var color = document.getElementById("color-picker");
+//   document.getElementById("about").style.background = color.value;
+//   document.getElementById("hero").style.background = color.value;
+// }
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".text", {
+    strings: [
+      "Front-End Developer",
+      "Web Developer",
+      "Aspiring UI/UX Designer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+});
+
+// front-end engineers, front-end web developers, JavaScript Developers, HTML/CSS Developer, front-end web designers, and front-end web architects.
